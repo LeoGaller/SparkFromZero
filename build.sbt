5cb0fe922c8f7f4b66f355f67da707fdce0a7cfa
Leonardo Lacerda Galler <leolacerdagaller@live.com>
2022-06-20 00:31:12 -0300

    Updated Scala and Configurations

--- build.sbt
+++ build.sbt
@@ -1,11 +1,11 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.12.16"
+ThisBuild / scalaVersion := "2.12.15"
 
 lazy val root = (project in file("."))
   .settings(
     name := "SparkFromZero"
   )
 // Adding spark dependencies
-libraryDependencies += "org.apache.spark" %% "spark-core" % "3.3.0"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.3.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.1"
--- src/main/scala/QuickStart/sparkQuickStart.scala
+++ src/main/scala/QuickStart/sparkQuickStart.scala
@@ -1,13 +1,20 @@
 package QuickStart
-
 import org.apache.spark.sql.SparkSession
 
+// Self-contained application
 object sparkQuickStart extends App {
   val spark = SparkSession
     .builder()
     .master("spark://172.19.0.5:7077")
-    .appName("Spark SQL basic example")
-    .config("spark.some.config.option", "some-value")
+    .appName("Spark - Quick Start")
+    //.config("spark.some.config.option", "some-value")
     .getOrCreate()
+  val logFile = "/opt/workspace/data/README.md"
+  val logData = spark.read.textFile(logFile).cache()
+  val numAs = logData.filter(line => line.contains("a")).count()
+  val numBs = logData.filter(line => line.contains("b")).count()
+  println(s"Lines with a: $numAs, Lines with b: $numBs")
+  // Spark’s primary abstraction is a distributed collection of items called a Dataset. Datasets can be created from
+  // Hadoop InputFormats (such as HDFS files) or by transforming other Datasets.
 
 }
